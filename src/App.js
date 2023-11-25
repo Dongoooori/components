ded334a92e04ecfca7f4f78ed004d5bca02ff604
シン・ドングン <>
2023-11-25 14:44:02 +0900

    delete date

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Autokana from './components/autokana';
 import MyDate from './components/date';
-import Date from './components/date';
 import Images from './components/images';
 import Query from './components/query';
 
