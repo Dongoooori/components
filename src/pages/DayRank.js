1971d23359b6124257132a1d086a0c07db24b540
シン・ドングン <>
2023-12-09 15:01:17 +0900

    conflict retry

--- src/pages/DayRank.js
+++ src/pages/DayRank.js
@@ -3,9 +3,6 @@ export default function DayRank() {
     console.log("conflict")
     console.log("conflict")
     console.log("conflict")
-    console.log("conflict")
-    console.log("conflict")
-    console.log("conflict")
 
   return (
     <div>DayRank</div>
