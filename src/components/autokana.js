63fe308ec04d7b44c76d829821e879ea148c0378
シン・ドングン <>
2024-01-27 12:58:28 +0900

    rebase

--- src/components/autokana.js
+++ src/components/autokana.js
@@ -1,5 +1,4 @@
-import { TextField } from "@mui/material";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import * as AutoKana from 'vanilla-autokana';
 
 
